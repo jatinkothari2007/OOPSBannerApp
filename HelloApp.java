e434db8f4a9ff17e226fca443403eeded7ca0365
jatinkothari2007 <jatinkothari2007@gmail.com>
2026-03-26 10:09:17 +0530

    UC3: Display Hello using String.join()

--- HelloApp.java
+++ HelloApp.java
@@ -3,8 +3,8 @@ public class HelloApp {
     public static void main(String[] args) {
 
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
+            String message = String.join("", "Hello, ", args[0], "!");
+            System.out.println(message);
         } else {
             System.out.println("Hello, World!");
         }
